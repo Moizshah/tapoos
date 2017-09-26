af527655dc2b0aa46bbcd8c13e1c74f9b629d81d
Atif Riaz <atif.riaz.dev@gmail.com>
2017-09-26 15:53:53 +0500

    google analytics Api changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -88,7 +88,7 @@
         <script src="{{ url('assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
         <!-- END THEME GLOBAL SCRIPTS -->
         <!-- BEGIN PAGE LEVEL SCRIPTS -->
-        <script src="{{ url('assets/pages/scripts/login.min.js') }}" type="text/javascript"></script>
+        <script src="{{ url('assets/pages/scripts/login.min.js')}}" type="text/javascript"></script>
         <!-- END PAGE LEVEL SCRIPTS -->
         <!-- BEGIN THEME LAYOUT SCRIPTS -->
         <!-- END THEME LAYOUT SCRIPTS -->
